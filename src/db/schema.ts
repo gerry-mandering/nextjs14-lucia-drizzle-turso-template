6ecb14d23cedc78f47ea8f84026ed96ac27a8de7
gerry-mandering <kimmen1119@gmail.com>
2024-08-04 13:09:17 +0900

    Feat: add ThemeProvider

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,76 +1,15 @@
-import { integer, text, sqliteTableCreator } from "drizzle-orm/sqlite-core";
+import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const accountTypeEnum = ["email", "google", "github"] as const;
-
-const sqliteTable = sqliteTableCreator(name => `app_${name}`);
-
-export const users = sqliteTable("users", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  email: text("email").unique(),
-  emailVerified: integer("email_verified", { mode: "timestamp" }),
+export const userTable = sqliteTable("user", {
+  id: text("id").notNull().primaryKey(),
+  username: text("username").notNull().unique(),
+  password_hash: text("password_hash").notNull(),
 });
 
-export const accounts = sqliteTable("accounts", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  userId: integer("user_id", { mode: "number" })
-    .references(() => users.id, { onDelete: "cascade" })
-    .unique()
-    .notNull(),
-  accountType: text("account_type", { enum: accountTypeEnum }).notNull(),
-  githubId: text("github_id").unique(),
-  googleId: text("google_id").unique(),
-  password: text("password"),
-  salt: text("salt"),
-});
-
-export const magicLinks = sqliteTable("magic_links", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  email: text("email").notNull().unique(),
-  token: text("token"),
-  tokenExpiresAt: integer("token_expires_at", { mode: "timestamp" }).notNull(),
-});
-
-export const resetTokens = sqliteTable("reset_tokens", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  userId: integer("user_id", { mode: "number" })
-    .references(() => users.id, { onDelete: "cascade" })
-    .unique()
-    .notNull(),
-  token: text("token"),
-  tokenExpiresAt: integer("token_expires_at", { mode: "timestamp" }).notNull(),
-});
-
-export const verifyEmailTokens = sqliteTable("verify_email_tokens", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  userId: integer("user_id", { mode: "number" })
-    .references(() => users.id, { onDelete: "cascade" })
-    .unique()
-    .notNull(),
-  token: text("token"),
-  tokenExpiresAt: integer("token_expires_at", { mode: "timestamp" }).notNull(),
-});
-
-export const profiles = sqliteTable("profile", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  userId: integer("user_id", { mode: "number" })
-    .references(() => users.id, { onDelete: "cascade" })
-    .unique()
-    .notNull(),
-  displayName: text("display_name"),
-  imageId: text("image_id"),
-  image: text("image"),
-  bio: text("bio").notNull().default(""),
-});
-
-export const sessions = sqliteTable("session", {
-  id: text("id").primaryKey(),
-  userId: integer("user_id", { mode: "number" })
-    .references(() => users.id, {
-      onDelete: "cascade",
-    })
-    .notNull(),
+export const sessionTable = sqliteTable("session", {
+  id: text("id").notNull().primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
   expiresAt: integer("expires_at").notNull(),
 });
-
-export type User = typeof users.$inferSelect;
-export type Profile = typeof profiles.$inferSelect;
